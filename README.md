c859e543a3a5ad504d42f5e29e5372f95c3fd79d
AngleOpera <Angle0p3r4@gmail.com>
2025-03-02 10:10:44 -0800

    Begin MainMenu

--- README.md
+++ README.md
@@ -4,3 +4,6 @@
 
 [Join the Experience](https://ro.blox.com/Ebh5?af_dp=roblox%3A%2F%2FplaceId%3D129543241385348%26launchData%3Dutm1%25253A0%25252C0%25252Cweb-link%25252Chome-hero-play-button%25252C%25253B&af_web_dp=https%3A%2F%2Fwww.roblox.com%2Fgames%2Fstart%3FplaceId%3D129543241385348%26launchData%3Dutm1%25253A0%25252C0%25252Cweb-link%25252Chome-hero-play-button%25252C%25253B) (BETA)!
 
+## Setup
+
+- Install aftman https://github.com/LPGhatguy/aftman/releases
--- src/StarterPlayer/StarterPlayerScripts/Gui/sections/MainMenu.tsx
+++ src/StarterPlayer/StarterPlayerScripts/Gui/sections/MainMenu.tsx
@@ -1,50 +1,87 @@
-import React from '@rbxts/react'
+import React, { useState } from '@rbxts/react'
+import { useSelector } from '@rbxts/react-reflex'
 import { palette } from 'ReplicatedStorage/shared/constants/palette'
 import { fonts } from 'StarterPlayer/StarterPlayerScripts/fonts'
+import { useRem } from 'StarterPlayer/StarterPlayerScripts/Gui/hooks/useRem'
+import {
+  selectIsMenuOpen,
+  store,
+} from 'StarterPlayer/StarterPlayerScripts/store'
 
 export function MainMenu() {
+  const rem = useRem()
+  const opened = useSelector(selectIsMenuOpen)
+  const [playHover, setPlayHover] = useState(false)
+  const [storeHover, setStoreHover] = useState(false)
+  const [tradeHover, setTradeHover] = useState(false)
   return (
-    <screengui>
-      <frame
-        Position={new UDim2(0.2, 0, 0.2, 0)}
-        Size={new UDim2(0.8, 0, 0.8, 0)}
-        BackgroundColor3={palette.purple}
-        BorderColor3={palette.pink}
-        Transparency={0.3}
-      >
-        <uilistlayout
-          FillDirection="Vertical"
-          VerticalAlignment="Center"
-          HorizontalAlignment="Center"
-          SortOrder="LayoutOrder"
-        />
-        <textlabel
-          Text="Station A33"
-          FontFace={fonts.inter.regular}
-          TextColor3={palette.text}
-          LayoutOrder={1}
-        />
-        <textbutton
-          Text="Play"
-          FontFace={fonts.inter.regular}
-          LayoutOrder={2}
-          BackgroundColor3={palette.blue}
+    opened && (
+      <screengui>
+        <frame
+          Position={new UDim2(0.1, 0, 0.1, 0)}
+          Size={new UDim2(0.8, 0, 0.8, 0)}
+          BackgroundColor3={palette.purple}
+          BorderColor3={palette.pink}
+          Transparency={0.3}
         >
-          <uicorner CornerRadius={new UDim(3)} />
-        </textbutton>
-        <textbutton
-          Text="Store"
-          FontFace={fonts.inter.regular}
-          LayoutOrder={3}
-          BackgroundColor3={palette.blue}
-        />
-        <textbutton
-          Text="Trade"
-          FontFace={fonts.inter.regular}
-          LayoutOrder={4}
-          BackgroundColor3={palette.blue}
-        />
-      </frame>
-    </screengui>
+          <uicorner CornerRadius={new UDim(1)} />
+          <uilistlayout
+            FillDirection="Vertical"
+            VerticalAlignment="Center"
+            HorizontalAlignment="Center"
+            Padding={new UDim(0, rem(2))}
+            SortOrder="LayoutOrder"
+          />
+          <textlabel
+            Text="Station A33"
+            TextSize={rem(10)}
+            AutomaticSize="XY"
+            BackgroundTransparency={1}
+            FontFace={fonts.inter.regular}
+            TextColor3={palette.text}
+          />
+          <textbutton
+            Text="Play"
+            TextSize={rem(5)}
+            AutomaticSize="XY"
+            FontFace={fonts.inter.regular}
+            BackgroundColor3={playHover ? palette.green : palette.blue}
+            Event={{
+              Activated: () => store.setMenuOpen(false),
+              MouseEnter: () => setPlayHover(true),
+              MouseLeave: () => setPlayHover(false),
+            }}
+          >
+            <uicorner CornerRadius={new UDim(1)} />
+          </textbutton>
+          <textbutton
+            Text="Store"
+            TextSize={rem(5)}
+            AutomaticSize="XY"
+            FontFace={fonts.inter.regular}
+            BackgroundColor3={storeHover ? palette.green : palette.blue}
+            Event={{
+              MouseEnter: () => setStoreHover(true),
+              MouseLeave: () => setStoreHover(false),
+            }}
+          >
+            <uicorner CornerRadius={new UDim(1)} />
+          </textbutton>
+          <textbutton
+            Text="Trade"
+            TextSize={rem(5)}
+            AutomaticSize="XY"
+            FontFace={fonts.inter.regular}
+            BackgroundColor3={tradeHover ? palette.green : palette.blue}
+            Event={{
+              MouseEnter: () => setTradeHover(true),
+              MouseLeave: () => setTradeHover(false),
+            }}
+          >
+            <uicorner CornerRadius={new UDim(1)} />
+          </textbutton>
+        </frame>
+      </screengui>
+    )
   )
 }
